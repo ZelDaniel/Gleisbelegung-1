23e5ced198f74418968a3c6f3d065df9ab599e1b
greonyral <nelphindal@gmail.com>
2018-12-31 12:49:19 +0100

    Reformat and bugfix to prime test

--- src/main/java/org/gleisbelegung/utils/MathX.java
+++ src/main/java/org/gleisbelegung/utils/MathX.java
@@ -7,63 +7,59 @@ import java.util.TreeSet;
 
 public class MathX {
 
-	private static final Set<Integer> primes = new TreeSet<>();
-	private static int nextCandidate = 2; // next number to check for prime
-	private static int maxDiv = 2; // smallest integer that 2 * maxDiv = nextCandidate 
+    private static final Set<Integer> primes = new TreeSet<>();
+    private static int nextCandidate = 2; // next number to check for prime
+    private static int maxDiv = 1; // smallest integer that 2 * maxDiv = nextCandidate
 
-	public static boolean isPrime(int i) {
-		if (i < 0)
-			throw new IllegalArgumentException(
-					"Argument needs to be non negative integer");
-		if (i < 2)
-			return false;
+    public static boolean isPrime(int i) {
+        if (i < 0) {
+            throw new IllegalArgumentException(
+                    "Argument needs to be non negative integer");
+        }
+        if (i < 2 || i < nextCandidate) {
+            return false;
+        }
 
-		if (primes.isEmpty()) {
-			primes.add(Integer.valueOf(nextCandidate++));
-		}
-		if (i < nextCandidate)
-			return false;
+        /*
+         * test prime and fill lookup list with all numbers starting from nextCandidate
+         */
+        while (nextCandidate <= i) {
+            if (maxDiv * 2 < nextCandidate) {
+                maxDiv++;
+            }
+            int div = maxDiv;
+            boolean isPrime = true;
+            int prime = 0;
+            Iterator<Integer> primes = MathX.primes.iterator();
+            while (primes.hasNext()) {
+                prime = primes.next().intValue();
+                /*
+                 *  is prime > sqrt(nextCandidate)
+                 *  if yes nextCandidate can't be prime anymore
+                 */
+                if (prime * prime > nextCandidate) {
+                    break;
+                }
+                int product = prime * div;
+                if (product != nextCandidate) {
+                    div = nextCandidate / prime; // integer division is sufficient here
+                    product = prime * div;
+                }
+                if (product == nextCandidate) {
+                    isPrime = false;
+                    break;
+                }
+            }
+            if (isPrime) {
+                MathX.primes.add(nextCandidate);
+            }
+            nextCandidate++;
+        }
 
-		/*
-		 * test prime and fill lookup list with all numbers starting from nextCandidate
-		 */
-		while (nextCandidate <= i) {
-			if (maxDiv * 2 < nextCandidate) {
-				maxDiv++;
-			}
-			int div = maxDiv;
-			boolean isPrime = true;
-			int prime = 0;
-			Iterator<Integer> primes = MathX.primes.iterator();
-			while (primes.hasNext()) {
-				prime = primes.next().intValue();
-				/*
-				 *  is prime > sqrt(nextCandidate)
-				 *  if yes nextCandidate can't be prime anymore
-				 */
-				if (prime * prime > nextCandidate) {
-					break;
-				}
-				int product = prime * div;
-				while (product > nextCandidate) {
-					div--;
-					product = prime * div;
-				}
-				if (product == nextCandidate) {
-					isPrime = false;
-					break;
-				}
-			}
-			if (isPrime) {
-				MathX.primes.add(nextCandidate);
-			}
-			nextCandidate++;
-		}
+        /*
+         * lookup
+         */
+        return MathX.primes.contains(Integer.valueOf(i));
 
-		/*
-		 * lookup
-		 */
-		return MathX.primes.contains(Integer.valueOf(i));
-
-	}
+    }
 }
--- src/test/java/org/gleisbelegung/utils/MathXTest.java
+++ src/test/java/org/gleisbelegung/utils/MathXTest.java
@@ -12,5 +12,14 @@ public class MathXTest
         assertTrue(MathX.isPrime(3));
         assertTrue(MathX.isPrime(5));
         assertFalse(MathX.isPrime(4));
+        assertFalse(MathX.isPrime(25));
+        assertFalse(MathX.isPrime(1024));
+        assertFalse(MathX.isPrime(2 ^ 30 - 1));
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testIsPrimeNegative()
+    {
+        MathX.isPrime(-1);
     }
 }

36f47b3def7c44d857790a0d15cc0da290e74bc6
Manuel <manuel-serret@t-online.de>
2019-01-16 17:22:43 +0100

    Button: fire event on Enter Keypress

--- src/main/java/org/gleisbelegung/ui/launch/MainPanel.java
+++ src/main/java/org/gleisbelegung/ui/launch/MainPanel.java
@@ -49,10 +49,10 @@ public class MainPanel implements PanelInterface {
             connect.getNode().setDisable(true);
             plugin.tryConnection(hostAddress);
         };
-        connect = ButtonFactory.create("Verbinden", 16, runnableConnect);
+        connect = ButtonFactory.create("Verbinden", 16, runnableConnect, true);
 
         Runnable runnableClose = Platform::exit;
-        close = ButtonFactory.create("Beenden", 16, runnableClose);
+        close = ButtonFactory.create("Beenden", 16, runnableClose, true);
 
         NodeWrapper<Pane> pane = new NodeWrapper<>(new Pane(
                 connectionLabel.getNode(), host.getNode(), close.getNode(), connect.getNode()
--- src/main/java/org/gleisbelegung/ui/lib/node/ButtonFactory.java
+++ src/main/java/org/gleisbelegung/ui/lib/node/ButtonFactory.java
@@ -1,6 +1,8 @@
 package org.gleisbelegung.ui.lib.node;
 
 import javafx.scene.control.Button;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.text.Font;
 import org.gleisbelegung.ui.lib.style.NodeWrapper;
 
@@ -16,26 +18,35 @@ public class ButtonFactory {
      * @return itialized and styled JavaFX Button
      */
     public static NodeWrapper<Button> create(String text, int fontSize,
-            Runnable onClick) {
+            Runnable onClick, boolean executeRunnableOnEnter) {
         Button b = new Button(text);
         b.setFont(Font.font(fontSize));
         b.getStylesheets().add("ui/nodes/button.css");
-        
+
         if (onClick != null) {
             b.setOnAction((e) -> onClick.run());
+        }
+
+        if(executeRunnableOnEnter){
+            b.addEventHandler(KeyEvent.KEY_PRESSED, ev -> {
+                if (ev.getCode() == KeyCode.ENTER) {
+                    b.fire();
+                    ev.consume();
+                }
+            });
         }
 
         return new NodeWrapper<>(b);
     }
 
     /**
-     * {@link ButtonFactory#create(String, int, Runnable)}
+     * {@link ButtonFactory#create(String, int, Runnable, boolean)}
      *
      * @param text
      * @param fontSize
      * @return
      */
     public static NodeWrapper<Button> create(String text, int fontSize) {
-        return create(text, fontSize, null);
+        return create(text, fontSize, null, false);
     }
 }
